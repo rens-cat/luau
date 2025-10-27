d74d90ff637273e5af72293c3a94756dc40852e2
rens-cat <83489381+rens-cat@users.noreply.github.com>
2025-10-26 18:13:41 -0700

    Create ObbyComplete.lua

---
+++ ObbyComplete.lua
@@ -0,0 +1,35 @@
+-- some games dont work idk why should work usually
+-- enjoy ur stupid obby script
+print("fiji made this guys FAHHH\n Some games dont work sorry!")
+
+------------------------------- Dont touch beyond here.
+
+local player = game.Players.LocalPlayer
+
+local character = player.Character or player.CharacterAdded:Wait()
+local rootPart = character:WaitForChild("HumanoidRootPart")
+
+local folder = workspace:FindFirstChild("Checkpoints")
+if not folder then
+    print("you dont have the correct folder where the checkpoints are stored inside of the game. look thru the explorer.")
+    return
+end
+
+print("found the folder starting tping in 3 seconds")
+wait(3)
+
+for i = 1, 1000 do
+    local checkpoint = folder:FindFirstChild(tostring(i))
+    if checkpoint then
+        rootPart.Position = checkpoint.Position + Vector3.new(0, 3, 0)
+        wait(TimeTillTp)
+    else
+        break
+    end
+end
+
+print("script completed")
+
+-- fijidacherry on discord made this script for your entertainment! <3
+-- dont try to execute this on anything that isnt matcha lol idk if itll work
+-- k thx bye
