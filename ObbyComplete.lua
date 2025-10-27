bb258e77d02ca967a32ffd04c33ac69f9e9c9daa
rens-cat <83489381+rens-cat@users.noreply.github.com>
2025-10-26 18:26:39 -0700

    Update ObbyComplete.lua

--- ObbyComplete.lua
+++ ObbyComplete.lua
@@ -1,7 +1,6 @@
 -- some games dont work idk why should work usually
 -- enjoy ur stupid obby script
 print("fiji made this guys FAHHH")
-
 ------------------------------- Dont touch beyond here.
 
 local player = game.Players.LocalPlayer
@@ -18,9 +17,8 @@ end
 print("found the folder starting tping in 3 seconds")
 wait(3)
 
-print("starting now stinky cheater smh")
 for i = 1, 1000 do
-    local checkpoint = path:FindFirstChild(tostring(i))
+    local checkpoint = folder:FindFirstChild(tostring(i))
     if checkpoint then
         rootPart.Position = checkpoint.Position + Vector3.new(0, 3, 0)
         wait(TimeTillTp)
